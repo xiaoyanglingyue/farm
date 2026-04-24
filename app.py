72d18a7cd674a1d3ca60b315fecb63d1d6ad841a
xiaoyanglingyue <xiaoyanglingyue@example.com>
2026-04-24 13:55:50 +0800

    更新新加功能

--- app.py
+++ app.py
@@ -3239,6 +3239,8 @@ allowed_origins = [
     "http://127.0.0.1:8001",
     "http://localhost:3000",  # 前端开发服务器
     "http://127.0.0.1:3000",
+    "https://servicewechat.com",          # 小程序开发者工具
+    "http://192.168.1.100:8001",
 ]
 
 app.add_middleware(
